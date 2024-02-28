488d7008c205d303570622600c36fa1758c1cb1c
warmmark <zhongli.goldheart@gmail.com>
2024-02-28 10:24:53 +0300

    greeting upgrade

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-import greeting from '../src/cli.js';
+import { getname, greeting } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 
-console.log(greeting());
+const userName = getname();
+
+console.log(greeting(userName));
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default function greeting() {
-  const userName = readlineSync.question('May I have your name? ');
+export function getname() {
+  return readlineSync.question('May I have your name? ');
+}
+
+export function greeting(userName) {
   return `Hello, ${userName}!`;
 }
